6ff0d3ab4ea7b94962dada126a0ededb42eb03b0
Abdelrahman-Alkhodary <abdelrahman.helmy2010@gmail.com>
2023-08-04 22:49:13 +0300

    colab file added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Text-Summarizer
+
+This project is done following the tutorial in https://www.youtube.com/watch?v=p7V4Aa7qEpw
--- template.py
+++ template.py
@@ -25,7 +25,7 @@ list_of_files = [
     "Dockerfile",
     "requirements.txt",
     "setup.py",
-    "research/tirals,ipynb",
+    "research/tirals.ipynb",
 ]
 
 for filepath in list_of_files:
